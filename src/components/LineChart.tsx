2033edf1c652218395f929b63188c57684379325
Kamil Majkrzak <kamilmajkrzak@kamils-mbp-1.home>
2024-05-16 15:56:57 +0200

    LineChart changes

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { useHistoricalPrice } from '../contexts/HistoricalPriceContext';
 import { Chart, LineController, LineElement, PointElement, LinearScale, Title, Tooltip, Filler, Legend, CategoryScale } from 'chart.js';
+import { Period } from '../types/apiTypes'
 
 Chart.register(LineController, LineElement, PointElement, LinearScale, Title, Tooltip, Filler, Legend, CategoryScale);
 
 const LineChart = () => {
 	const chartRef = useRef<Chart<"line", number[], unknown> | null>(null);
-	const { data, isLoading, error } = useHistoricalPrice();
+	const { data, isLoading, error, period } = useHistoricalPrice();
 	const [windowSize, setWindowSize] = useState(window.innerWidth);
+	const [initialLoad, setInitialLoad] = useState(true);
 
 	const handleResize = () => {
 		setWindowSize(window.innerWidth);
 	};
+
+	useEffect(() => {
+		if (data && initialLoad && !isLoading) {
+			setInitialLoad(false);
+		}
+	}, [data, isLoading]);
 
 	useEffect(() => {
 		window.addEventListener('resize', handleResize);
@@ -23,9 +31,19 @@ const LineChart = () => {
 	}, []);
 
 	useEffect(() => {
-		const chart = chartRef.current;
-		if (chart && chart.ctx) {
-			const ctx = chart.ctx;
+		if (!isLoading && data && chartRef.current) {
+			const chart = chartRef.current;
+			chart.data.labels = data.map(item => formatDate(item.timestamp, period));
+			chart.data.datasets.forEach((dataset) => {
+				dataset.data = data.map(item => item.price);
+			});
+			chart.update();
+		}
+	}, [data, period, isLoading]);
+
+	useEffect(() => {
+		if (chartRef.current && data) {
+			const ctx = chartRef.current.ctx;
 			const canvasHeight = ctx.canvas.clientHeight;
 			const gradientStroke = ctx.createLinearGradient(0, 0, 0, canvasHeight);
 			gradientStroke.addColorStop(0, '#01C38D80');
@@ -33,28 +51,49 @@ const LineChart = () => {
 			gradientStroke.addColorStop(0.35, '#01C38D80');
 			gradientStroke.addColorStop(1, 'transparent');
 
-			if (chart.data.datasets.length) {
-				chart.data.datasets[0].backgroundColor = gradientStroke;
-				chart.update();
-			}
+			chartRef.current.data.datasets.forEach(dataset => {
+				dataset.backgroundColor = gradientStroke;
+			});
+			chartRef.current.update();
 		}
-	}, [data, windowSize]);
+	}, [chartRef.current, data, windowSize]);
 
-	if (isLoading) return <p>Loading chart data...</p>;
 	if (error) return <p>Error loading chart data: {error.message}</p>;
-	if (!data) return <p>No data available</p>;
+	if (isLoading && initialLoad) return <p>Loading chart data...</p>;
+	if (!data) return <p>Loading chart data...</p>;
 
 	const prices = data.map(item => item.price);
 	const minPrice = Math.min(...prices);
 	const maxPrice = Math.max(...prices);
 	const stepSize = (maxPrice - minPrice) / 7;
 
+	const formatDate = (dateString: string, period: Period) => {
+		const date = new Date(dateString);
+		switch (period) {
+			case 'day':
+				return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+			case 'month':
+				return date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
+			case 'year':
+				return date.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit' });
+			case 'all':
+				return date.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit' });
+			default:
+				return date.toLocaleDateString('en-US');
+		}
+	};
+
+	const formattedData = data.map(item => ({
+		timestamp: formatDate(item.timestamp, period),
+		price: item.price
+	}));
+
 	const chartData = {
-		labels: data.map(item => new Date(item.timestamp).toLocaleTimeString()),
+		labels: formattedData.map(item => item.timestamp),
 		datasets: [
 			{
 				label: 'Price over Time',
-				data: prices,
+				data: formattedData.map(item => item.price),
 				borderColor: '#01C38D',
 				backgroundColor: '#191D27',
 				fill: true,
@@ -70,15 +109,16 @@ const LineChart = () => {
 			y: {
 				beginAtZero: false,
 				position: 'right' as const,
-				min: minPrice,
-				max: maxPrice,
+				// min: minPrice,
+				// max: maxPrice,
 				ticks: {
-					stepSize,
+					// stepSize,
 					maxTicksLimit: 5,
 					color: 'white',
 					font: {
-						family: 'Arial',
+						family: 'TT Commons',
 					},
+					// padding: -20,
 				},
 				grid: {
 					color: '#8F96A133',
@@ -97,9 +137,18 @@ const LineChart = () => {
 					maxTicksLimit: 6,
 					color: 'white',
 					font: {
-						family: 'Arial',
-					}
+						family: 'TT Commons',
+					},
+					// padding: -20,
 				}
+			}
+		},
+		layout: {
+			padding: {
+				left: 0,
+				right: 0,
+				top: 0,
+				bottom: 0
 			}
 		},
 		plugins: {
