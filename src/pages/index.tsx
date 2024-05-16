055b3058c756ddc18cadc9a74835c3dd3a05bdbc
Kamil Majkrzak <kamilmajkrzak@kamils-mbp-2.home>
2024-05-16 23:30:13 +0200

    Title length

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ import SEO from '../components/SEO'
 
 const IndexPage: React.FC<PageProps> = () => {
 	const seoData = {
-		title: 'BORG Token Metrics',
+		title: 'BORG Token Metrics - Comprehensive Overview',
 		description: 'Deep-dive into the statistics of BORG and the mechanics of the full SwissBorg Ecosystem.',
 		url: 'https://gentle-faloodeh-4e50e9.netlify.app',
 		twitterUsername: '@elonmusk',
